c5c80a3d9256b9ab4739dae8d9d1f9a09a6294a2
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-02-15 01:56:55 -0800

    TODO add routes for Agent and Client
    Add  routes to user and Client with new index page for Agent and Client Component

---
+++ src/components/Client/index.js
@@ -0,0 +1,12 @@
+
+import React, {Component} from 'react';
+import { withFirebase } from '../Firebase';
+
+const ClientPage = () => (
+  <div>
+    <h1>Client Page</h1>
+    <p> The Client Page is accessible by every client.</p>
+  </div>
+);
+
+export default (ClientPage);
