1ccf5684294e6b138fd07c8322f9d0fa9129d0b3
doremon0710 <doremon0710@gmail.com>
2019-02-17 16:56:00 -0800

    add config.js file
    add config.js file

---
+++ src/Footer/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+class Footer extends React.Component {
+    render(){
+      return (
+        <div>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style/footer.css"/>
+        <link rel="stylesheet" href="style/demo.css"/>
+        <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"/>
+        <link rel="stylesheet" type="text/css" href="style/homepage.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"/>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
+    
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    
+    
+    
+    <footer class="footer-distributed">
+    <div class="footer-left">
+      <h3>360Degree <span>House</span></h3>
+    
+      <p class="footer-links">
+        <a href="#">Home</a> 
+        <a href="#">Blog</a>  
+        <a href="#">About</a>
+        <a href="#">Faq</a>
+        <a href="#">Contact</a>
+      </p>
+    
+      <p class="footer-company-name">360DegreeHouse &copy; 2018</p>
+    </div>
+    
+    <div class="footer-center">
+    
+      <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>3637 Snell Avenue SPC364</span> San Jose, United States</p>
+      </div>
+    
+      <div>
+        <i class="fa fa-phone"></i>
+        <p>+1 408 466 5588</p>
+      </div>
+    
+      <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:support@company.com">support@360DegreeHouse.com</a></p>
+      </div>
+    
+    </div>
+    
+    <div class="footer-right">
+    
+      <p class="footer-company-about">
+        <span>About the company</span>
+        We are an inspiring group of house dealers.
+      </p>
+    
+      <div class="footer-icons">
+    
+        <a href="#"><i class="fab fa-facebook-square"></i></a>
+        <a href="#"><i class="fab fa-twitter-square"></i></a>
+        <a href="#"><i class="fab fa-linkedin"></i></a>
+    
+      </div>
+    </div>
+    </footer>
+    </div>    
+      );
+    }
+}
+
+export default Footer;
+
+
+
+
+
+
+
+
+
+
+
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import footer from '../Footer'
+
 
 const HomePage = () => (
   <div>
-    <h1>Home Page</h1>
-    <p> The Home Page is accessible by every user.</p>
+    <Home />
+    <footer />
   </div>
+);
+
+const Home = () => (
+  <div>
+  <h1>Home Page</h1>
+  <p> The Home Page is accessible by every user.</p>
+</div>
 );
 
 export default (HomePage);
