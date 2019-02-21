7fe3dac2759b498d5e211c77b47b05d1df27d1f8
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-02-21 00:05:10 -0800

    add firebase house ref

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -54,6 +54,12 @@ class Firebase {
 
   users = () => this.database.ref('users');
 
+  // Collection API
+
+  house = uid => this.database.ref(`houses/${uid}`);
+
+  houses = () => this.database.ref('houses');
+
   // Sign Up Email Verification
 
   doSendEmailVerification = () =>
