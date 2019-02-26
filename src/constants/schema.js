6db64a461d63d0217e448929f508aeac3746407f
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-02-25 19:45:49 -0800

    add Schema for user and house

---
+++ src/constants/schema.js
@@ -0,0 +1,27 @@
+const user = {
+  displayName: '',
+  email: '',
+  gender: '',
+  phoneNumber: '',
+  isAgent: false,
+  photoURL: '',
+}
+
+const house = {
+  address: '',
+  price: '',
+  description: '',
+  data: '',
+  features: {
+    type: '',
+    yearBuild: '',
+    heating: '',
+    cooling: '',
+    parking: '',
+    lot: '',
+  },
+  agentId: [],
+  userID: [],
+}
+
+export {user, house,}
